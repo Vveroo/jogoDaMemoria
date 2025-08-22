d24d02ad107297cd01fda1d1e1f17ae0dfb53460
sophiarb09 <sophiarb2009@gmail.com>
2025-08-21 23:16:52 -0300

    erro

--- script.js
+++ script.js
@@ -1,64 +1,8 @@
-const cartas = document.querySelector('.carta');
-const cartas = document.querySelector('.cartasMemoria');
-    const cartas = document.querySelectorAll('.carta');
 const cartas = document.querySelectorAll('.carta');
-
  
 let virada = false;
 let bloqueio = false; 
 let primeira, segunda;
-
-function virar() {
-    this.classList.add('virar');
-
-    if(!virada){
-        virada = true;
-        primeira = this;
-        return;
-    }
-
-    segunda = this;
-    virada = false;
-}
-
-function checarPares() {
-    if(primeira.dataset.framework === segunda.dataset.framework){
-        desabilitar();
-    }
-
-    else{
-        desvirar();
-    }
-}
-
-function desabilitar() {
-    primeira.removeEventLinstener('click', virar);
-    segunda.removeEventLinstener('click', virar);
-}
-
-function desvirar() {
-    setTimeout(() => {
-        primeira.classList.remove('virar');
-        segunda.classList.remove('virar');
-    }, 1500);
-}
-
-(function embaralhar() {
-    cartas.forEach(carta => {
-        let aleatório = Math.floor(Math.round () * 6);
-        carta.style.order = aleatório;
-    });
-})();
-
-cartas.forEach(carta => carta.addEventListene('click', virar));       const cartas = document.querySelectorAll('.carta');
-       const cartas = document.querySelectorAll('.carta');
-
-        let virada = false;
-        let bloqueio = false; 
-        let primeira, segunda;
-    let virada = false;
-    let bloqueio = false; 
-    let primeira, segunda;
 
         function virar() {
             if (bloqueio) return;
