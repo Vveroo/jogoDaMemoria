7857b61ab418ebe706ac8e3a9a2849fd41a9b0ee
sophiarb09 <sophiarb2009@gmail.com>
2025-08-21 22:52:53 -0300

    Jogo pronto!!!

--- script.js
+++ script.js
@@ -46,4 +46,63 @@ function desvirar() {
     });
 })();
 
-cartas.forEach(carta => carta.addEventListene('click', virar));
+cartas.forEach(carta => carta.addEventListene('click', virar));       const cartas = document.querySelectorAll('.carta');
+
+        let virada = false;
+        let bloqueio = false; 
+        let primeira, segunda;
+
+        function virar() {
+            if (bloqueio) return;
+            if (this === primeira) return;
+
+            this.classList.add('virada');
+
+            if (!virada) {
+                virada = true;
+                primeira = this;
+                return;
+            }
+
+            segunda = this;
+            bloqueio = true; 
+            checarPares();
+        }
+
+        function checarPares() {
+            if (primeira.dataset.framework === segunda.dataset.framework) {
+                desabilitar();
+            } else {
+                desvirar();
+            }
+        }
+
+        function desabilitar() {
+            primeira.removeEventListener('click', virar);
+            segunda.removeEventListener('click', virar);
+            resetarCartas();
+        }
+
+        function desvirar() {
+            setTimeout(() => {
+                primeira.classList.remove('virada');
+                segunda.classList.remove('virada');
+                resetarCartas();
+            }, 1500);
+        }
+
+        function resetarCartas() {
+            virada = false;
+            bloqueio = false;
+            primeira = null;
+            segunda = null;
+        }
+
+        (function embaralhar() {
+            cartas.forEach(carta => {
+                let aleatorio = Math.floor(Math.random() * cartas.length);
+                carta.style.order = aleatorio;
+            });
+        })();
+
+        cartas.forEach(carta => carta.addEventListener('click', virar));
